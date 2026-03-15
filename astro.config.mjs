1af6263778c1ea5714053820f294e1ce55f7bdc9
XuanChenAI <xuanchenyiran@163.com>
2026-03-15 16:26:43 +0800

    Add more resources to all pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,12 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static'
+  output: 'static',
+  base: '/xuanchen_astro',
+  vite: {
+    build: {
+      cssMinify: true,
+      assetsInlineLimit: 100000
+    }
+  }
 });
